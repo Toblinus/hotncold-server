739941c7f5e7ad25db1cd412fac28da6e93b7b67
Toblinus <anatoliy.blinkov3006@gmail.com>
2020-11-14 21:17:24 +0300

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,24 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
+let clients1 = null;
+let clients2 = null;
+
 wss.on('connection', (ws) => {
     console.log('Client connected');
+    if(clients1 == null){
+        clients1 = ws;
+        ws.on("message", (message) => {
+            if(clients2 == null) return;
+            clients2.send(message);
+        });
+    } else {
+        clients2 = ws;
+        ws.on("message", (message) => {
+            if(clients1 == null) return;
+            clients1.send(message);
+        });
+    }
+
     ws.on('close', () => console.log('Client disconnected'));
-  });
+});
